c0ac1b74c544cb770b9eb437f2451284dc22a1cc
Pokka <ukfake1990@gmail.com>
2024-12-16 00:04:58 +0200

    Update clean.js

--- clean.js
+++ clean.js
@@ -4,20 +4,13 @@
   Lampa.Listener.follow("app", (e) => {
     if (e.type == "ready") {
       setTimeout(function () {
-        $("[data-action=anime]").eq(0).remove();
-        $("[data-action=feed]").eq(0).remove();
-        $("[data-action=myperson]").eq(0).remove();
-        $("[data-action=about]").eq(0).remove();
-        $("[data-action=subscribes]").eq(0).remove();
-        $("[data-action=catalog]").eq(0).remove();
-        $("[data-action=relise]").eq(0).remove();
-        $("[data-action=mytorrents]").eq(0).remove();
-        [".open--premium", ".open--feed", ".open--notice", ".open--profile"].forEach(function (a) {
-          return $(a).hide();
+        
+        ["anime", "feed", "myperson", "about", "subscribes", "catalog", "relise", "mytorrents"].forEach(function (a) {
+          return $("[data-action=" + a + "]").eq(0).hide();
         });
-
-        [".head__split"].forEach(function (a) {
-          (document.querySelector(a).style.width = 0), (document.querySelector(a).style.margin = "0 0.7em");
+        
+        [".open--premium", ".open--feed", ".open--notice", ".open--profile", ".full-screen", ".open--settings"].forEach(function (a) {
+          return $(".head__action" + a).hide();
         });
       }, 10);
     }
